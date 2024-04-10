d99108dcf45eaf9128025e93bc972db61d8ab3c8
Gavin Brown <gavin.brown@fastmail.uk>
2024-04-10 15:43:38 +0100

    use alpine instead of Ubuntu. Install local datasets during the build process. clean up better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-rm -rf results
+help() {
+    cat << END
+Arguments:
+  [--timeout=<timeout>]
+  [[--use-rdap-profile-february-2019]
+  ([--gtld-registrar] | [--gtld-registry [--thin]])]
+  RDAP_URI
+END
+  exit 0
+}
 
-mkdir results
+[ "$1" = "" ] && help
 
-java -jar tool/bin/rdapct-1.0.jar -c config.json "$@" 1>&2
+for arg in "$@" ; do
+  if [ "--help" = "$arg" ] || [ "-h" = "$arg" ] ; then
+    help
+  fi
+done
 
-cat results/*
+java -jar tool/bin/rdapct-1.0.jar -c config.json --use-local-datasets "$@" 1>&2
+
+find results -type f -exec cat {} \; -delete
