a944cee5d86e633d5f05707f2a7c139d81fdd88f
Gavin Brown <gavin.brown@fastmail.uk>
2024-04-10 15:08:58 +0100

    updated

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rm -rf results/*
+rm -rf results
 
-java -jar tool/bin/rdapct-1.0.jar -c config.json "$1" 1>&2
+mkdir results
+
+java -jar tool/bin/rdapct-1.0.jar -c config.json "$@" 1>&2
 
 cat results/*
